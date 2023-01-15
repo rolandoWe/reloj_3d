a592cde809c58981988a1ff939cfbeb876c6e89d
Rolando <riverarolando71@gmail.com>
2023-01-15 17:44:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function dato(){
+    let datos=new Date();
+    let hora=datos.getHours()
+    let minuto=datos.getMinutes()
+    let dia=datos.getDay()
+    let diaFecha=datos.getDate()
+    let mes=datos.getMonth()
+    let anio=datos.getFullYear()
+    let am;
+    if(hora==12){
+        am='MD'
+    }else if(hora>12){
+        am='PM'
+    }else{
+        am='AM'
+    };
+    hora=hora>12? hora-12:hora;
+    hora=hora==0?hora=12:hora
+    minuto=minuto<10?'0'+minuto:minuto;
+
+    let diaSemana=['Dom','Lun','Mar','Mie','Jue','Vie','Sab'];
+    diaHoy=diaSemana[dia];
+
+    let meses=['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+    mesAct=meses[mes]
+
+    document.querySelector('.am').innerHTML=am;
+    document.querySelector('.mes_anio').innerHTML=mesAct+' '+ anio;
+    document.querySelector('.dia').innerHTML=diaHoy+' '+ diaFecha;
+
+    document.querySelector('.hora').innerHTML=hora+''+'<span class="puntos">:</span>'+''+minuto
+}
+setInterval(function(){
+    dato()
+
+},1000)
+
+function punt(){
+    document.querySelector('.puntos').classList.toggle('cli')
+}
+setInterval(function(){
+    punt()
+
+},2000)
+
+document.querySelector('.inspi').addEventListener('click',function(){
+    document.querySelector('.caja_demo').classList.toggle('ver_demo')
+})
+document.querySelector('.cerrar_demo').addEventListener('click',function(){
+    document.querySelector('.caja_demo').classList.toggle('ver_demo')
+})
